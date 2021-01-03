63b90f40cd94b1d831b61dd9150f88e030a62c35
Johnny Cao <jcao0928@gmail.com>
2021-01-03 03:19:20 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,7 +5,7 @@ def count_strings(array)
 end	
 
 
-def count_empty_strings(array)	def count_empty_strings(array)
+def count_empty_strings(array)	
  
   array.count do |element|
     if element.class == String 
